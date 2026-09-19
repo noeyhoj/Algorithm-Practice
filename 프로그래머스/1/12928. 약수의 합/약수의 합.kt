00fd2d8aa4450136e5903ce33ba0960aa8ad2d1b
김조현 <118670117+noeyhoj@users.noreply.github.com>
2026-09-19 20:16:24 +0900

    [level 1] Title: 약수의 합, Time: 2.03 ms, Memory: 59.5 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.kt
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.kt
@@ -1,9 +1,14 @@
+import kotlin.math.sqrt
+
 class Solution {
     fun solution(n: Int): Int {
-        var yacksus = mutableListOf<Int>()
+        var yacksus = mutableSetOf<Int>()
         if (n == 0) return 0
-        (1..n).forEach {
-            if (n % it == 0) yacksus.add(it)
+        (1..sqrt(n.toFloat()).toInt()).forEach {
+            if (n % it == 0) {
+                yacksus.add(it)
+                yacksus.add(n / it)
+            }
         }
         return yacksus.sumOf{ it }
     }
